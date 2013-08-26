44b93345cbb7be2204f70151a018b95a28bc8df6
maan81 <pranijman@gmail.com>
2013-08-26 16:19:45 +0545

    binding keybprd letters .....................

--- calculator.js
+++ calculator.js
@@ -14,6 +14,21 @@ function calc( disp){
 
 $(function(){
 
+
+	//bind keyboard letters
+	$(document).keyup(function(e){
+		switch(e.which){
+			case 48:case 49:case 50:case 51:case 52:
+			case 53:case 54:case 55:case 56:case 57:
+				$('#_'+e.which).trigger('click');
+				break;
+			default:
+				console.log('u pressed a key')
+		}
+	});
+
+
+
 	$('.number').click(function(){
 		var n = $(this).children('span').text();
 
@@ -23,10 +38,6 @@ $(function(){
 		disp += n;
 
 		display(disp);
-
-		buffer = eval(disp);
-
-
 	})
 
 
@@ -35,15 +46,77 @@ $(function(){
 		calc(disp);
 	})
 
-<div id="percent" class="basic_fn button tmp_color"><span>%</span></div>
-<div id="multiply" class="basic_fn button tmp_color"><span>&multiply;</span></div>
-<div id="plus" class="basic_fn button tmp_color"><span>+</span></div>
-<div id="divide" class="basic_fn button tmp_color"><span>&divide;</span></div>
-<div id="minus" class="basic_fn button tmp_color"><span>-</span></div>
-<div id="equals" class="basic_fn button tmp_color"><span>=</span></div>
+
+	$('#plus').click(function(){
+		disp += '+';
+		display(disp);
+	})
+	$('#minus').click(function(){
+		disp += '-';
+		display(disp);
+	})
 	$('#multiply').click(function(){
-		buffer 	
+		disp += '&multiply;';
+		display(disp);
+	})
+	$('#divide').click(function(){
+		disp += '&divide;';
+		display(disp);
+	})
+	$('#percent').click(function(){
+		disp += '%';
+		display(disp);
 	})
 
+
+
+	$('#sin').click(function(){
+		disp += 'Sin ';
+		display(disp);
+	})
+	$('#cos').click(function(){
+		disp += 'Cos ';
+		display(disp);
+	})
+	$('#tan').click(function(){
+		disp += 'Tan ';
+		display(disp);
+	})
+	$('#log').click(function(){
+		disp += 'Log ';
+		display(disp);
+	})
+	$('#e').click(function(){
+		disp += 'e ';
+		display(disp);
+	})
+	$('#hyp').click(function(){
+		disp += 'Hype ';
+		display(disp);
+	})
+	$('#sqrt').click(function(){
+		disp += 'Sqrt ';
+		display(disp);
+	})
+	$('#power').click(function(){
+		disp += '^ ';
+		display(disp);
+	})
+	$('#inv').click(function(){
+		disp += '-1 ';
+		display(disp);
+	})
+	$('#square').click(function(){
+		disp += '^2 ';
+		display(disp);
+	})
+	$('#open').click(function(){
+		disp += '( ';
+		display(disp);
+	})
+	$('#close').click(function(){
+		disp += ') ';
+		display(disp);
+	})
 })
 
